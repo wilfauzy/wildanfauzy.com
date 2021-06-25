9b18339eae7b0cee4bb7c252b4c7164bec6782ac
Wildan Fauzy <me@wildanfauzy.com>
2021-06-25 18:59:16 +0700

    Delete tweet.py

--- tweet.py
+++
@@ -1,9 +0,0 @@
-import tweepy
-auth = tweepy.OAuthHandler("kostumer key kalian", "kostumer secret kaliam") 
-auth.set_access_token("akses token kalian", "akses token secret kalian") 
-api = tweepy.API(auth)
-print ("Tweet From Terminal, Made By @WilFauzy On Twitter!")
-print ("Twitter For........")
-tweet = input("What Would You Like To Tweet? ")
-api.update_status(status =(tweet))
-print ("Done!")
