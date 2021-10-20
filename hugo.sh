6ad025a673ce57904d3e28533de5cd758536ddc0
wilfauzy <me@wildanfauzy.com>
2021-10-20 18:20:30 +0700

    6

--- hugo.sh
+++
@@ -1,4 +0,0 @@
-wget https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
-tar xvf hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
-chmod +x ./hugo
-./hugo --destination public
