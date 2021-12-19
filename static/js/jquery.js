68c55651228ccec204400071da198c704a734112
Wildan Fauzy <me@wildanfauzy.com>
2021-12-19 23:51:46 +0700

    Update jquery.js

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -1,28 +1,6 @@
-(() => {
-  'use strict';
-
-  let refOffset = 0;
-  const bannerHeight = 77;
-  const bannerWrapper = document.querySelector('.banner-wrapper');
-  const banner = document.querySelector('.banner');
-
-  const handler = () => {
-    const newOffset = window.scrollY || window.pageYOffset;
-
-    if (newOffset > bannerHeight) {
-      if (newOffset > refOffset) {
-        bannerWrapper.classList.remove('animateIn');
-        bannerWrapper.classList.add('animateOut');
-      } else {
-        bannerWrapper.classList.remove('animateOut');
-        bannerWrapper.classList.add('animateIn');
-      }
-      banner.style.background = 'rgba(162, 197, 35, 0.6)';
-      refOffset = newOffset;
-    } else {
-      banner.style.backgroundColor = 'rgba(162, 197, 35, 1)';
-    }
-  };
-
-  window.addEventListener('scroll', handler, false);
-})();
+var burgerMenu = document.getElementById('burger-menu');
+var overlay = document.getElementById('menu');
+burgerMenu.addEventListener('click',function(){
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
